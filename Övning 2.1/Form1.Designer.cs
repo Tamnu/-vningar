c7724fb97d9698d88925982b0574a3e0eec4cfdb
Samuel Arvidsson <samarv0104@skynet.halmstad.se>
2019-10-21 09:14:23 +0200

    Test

---
+++ Övning 2.1/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Övning_2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxTitle = new System.Windows.Forms.TextBox();
+            this.tbxSidor = new System.Windows.Forms.TextBox();
+            this.tbxTitleljud = new System.Windows.Forms.TextBox();
+            this.tbxTidljud = new System.Windows.Forms.TextBox();
+            this.tbxTitlefilm = new System.Windows.Forms.TextBox();
+            this.tbxTidfilm = new System.Windows.Forms.TextBox();
+            this.btnBok = new System.Windows.Forms.Button();
+            this.btnLjud = new System.Windows.Forms.Button();
+            this.btnFilm = new System.Windows.Forms.Button();
+            this.lbxVisa = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxUpplös = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxTitle
+            // 
+            this.tbxTitle.Location = new System.Drawing.Point(98, 57);
+            this.tbxTitle.Name = "tbxTitle";
+            this.tbxTitle.Size = new System.Drawing.Size(100, 20);
+            this.tbxTitle.TabIndex = 0;
+            // 
+            // tbxSidor
+            // 
+            this.tbxSidor.Location = new System.Drawing.Point(98, 112);
+            this.tbxSidor.Name = "tbxSidor";
+            this.tbxSidor.Size = new System.Drawing.Size(100, 20);
+            this.tbxSidor.TabIndex = 1;
+            // 
+            // tbxTitleljud
+            // 
+            this.tbxTitleljud.Location = new System.Drawing.Point(292, 57);
+            this.tbxTitleljud.Name = "tbxTitleljud";
+            this.tbxTitleljud.Size = new System.Drawing.Size(100, 20);
+            this.tbxTitleljud.TabIndex = 2;
+            // 
+            // tbxTidljud
+            // 
+            this.tbxTidljud.Location = new System.Drawing.Point(292, 112);
+            this.tbxTidljud.Name = "tbxTidljud";
+            this.tbxTidljud.Size = new System.Drawing.Size(100, 20);
+            this.tbxTidljud.TabIndex = 3;
+            // 
+            // tbxTitlefilm
+            // 
+            this.tbxTitlefilm.Location = new System.Drawing.Point(487, 57);
+            this.tbxTitlefilm.Name = "tbxTitlefilm";
+            this.tbxTitlefilm.Size = new System.Drawing.Size(100, 20);
+            this.tbxTitlefilm.TabIndex = 4;
+            // 
+            // tbxTidfilm
+            // 
+            this.tbxTidfilm.Location = new System.Drawing.Point(487, 94);
+            this.tbxTidfilm.Name = "tbxTidfilm";
+            this.tbxTidfilm.Size = new System.Drawing.Size(100, 20);
+            this.tbxTidfilm.TabIndex = 5;
+            // 
+            // btnBok
+            // 
+            this.btnBok.Location = new System.Drawing.Point(98, 187);
+            this.btnBok.Name = "btnBok";
+            this.btnBok.Size = new System.Drawing.Size(100, 23);
+            this.btnBok.TabIndex = 6;
+            this.btnBok.Text = "Registrera bok";
+            this.btnBok.UseVisualStyleBackColor = true;
+            // 
+            // btnLjud
+            // 
+            this.btnLjud.Location = new System.Drawing.Point(281, 187);
+            this.btnLjud.Name = "btnLjud";
+            this.btnLjud.Size = new System.Drawing.Size(130, 23);
+            this.btnLjud.TabIndex = 7;
+            this.btnLjud.Text = "Registrera ljudspår";
+            this.btnLjud.UseVisualStyleBackColor = true;
+            // 
+            // btnFilm
+            // 
+            this.btnFilm.Location = new System.Drawing.Point(487, 187);
+            this.btnFilm.Name = "btnFilm";
+            this.btnFilm.Size = new System.Drawing.Size(100, 23);
+            this.btnFilm.TabIndex = 8;
+            this.btnFilm.Text = "Registrera film";
+            this.btnFilm.UseVisualStyleBackColor = true;
+            // 
+            // lbxVisa
+            // 
+            this.lbxVisa.FormattingEnabled = true;
+            this.lbxVisa.Location = new System.Drawing.Point(77, 283);
+            this.lbxVisa.Name = "lbxVisa";
+            this.lbxVisa.Size = new System.Drawing.Size(652, 134);
+            this.lbxVisa.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Titel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Antal sidor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Titel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Speltid";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(446, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Title";
+            // 
+            // tbxUpplös
+            // 
+            this.tbxUpplös.Location = new System.Drawing.Point(487, 129);
+            this.tbxUpplös.Name = "tbxUpplös";
+            this.tbxUpplös.Size = new System.Drawing.Size(100, 20);
+            this.tbxUpplös.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(446, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Speltid";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(425, 132);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Upplösning";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(352, 247);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "BIBLOTEK";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(74, 18);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Bok";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(289, 18);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Ljudspår";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(484, 18);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(25, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Film";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbxUpplös);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxVisa);
+            this.Controls.Add(this.btnFilm);
+            this.Controls.Add(this.btnLjud);
+            this.Controls.Add(this.btnBok);
+            this.Controls.Add(this.tbxTidfilm);
+            this.Controls.Add(this.tbxTitlefilm);
+            this.Controls.Add(this.tbxTidljud);
+            this.Controls.Add(this.tbxTitleljud);
+            this.Controls.Add(this.tbxSidor);
+            this.Controls.Add(this.tbxTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxTitle;
+        private System.Windows.Forms.TextBox tbxSidor;
+        private System.Windows.Forms.TextBox tbxTitleljud;
+        private System.Windows.Forms.TextBox tbxTidljud;
+        private System.Windows.Forms.TextBox tbxTitlefilm;
+        private System.Windows.Forms.TextBox tbxTidfilm;
+        private System.Windows.Forms.Button btnBok;
+        private System.Windows.Forms.Button btnLjud;
+        private System.Windows.Forms.Button btnFilm;
+        private System.Windows.Forms.ListBox lbxVisa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxUpplös;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
