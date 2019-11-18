8855b502b2b90ff43dc2b9acfdc53b54f1c92e8c
Samuel Arvidsson <samarv0104@skynet.halmstad.se>
2019-11-18 09:39:49 +0100

    Nytt med uppgift 2.5

---
+++ Övning 2.5/Samling.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning_2._5
+{
+    class Samling
+    {
+    }
+}
+public class Samling<T>
+{
+    protected int buffert;
+    protected T[] lista;
+
+    protected int längd;
+    protected int antal;
+
+    public Samling()
+    {
+        buffert = 30;
+        antal = 0;
+        längd = 30;
+        lista = new T[längd];
+    }
+    protected void Expandera ( int storlek)
+    {
+        if (storlek < 1) return;
+
+        T[] temp = new T[längd + storlek];
+
+        for(int i = 0; i<antal; i++)
+        {
+            temp[i] = lista[i];
+            lista = temp;
+            längd += storlek;
+        }
+    }
+    protected void Reducera()
+    {
+        T[] temp = new T[antal];
+
+        for( int i = 0; i< antal; i++)
+        {
+            temp[i] = lista[i];
+            lista = temp;
+            längd = antal;
+        }
+    }
+    public void LäggTill(T e)
+    {
+
+    }
+}
